22b1f8d69dc0b9c00a16e9fece0ac18ed21f9934
Eddy Manoa Randrianarison <2433177@cegeplimoilou.ca>
2025-10-27 15:38:42 -0400

    Bootstrap

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
     $("#p1").hide();
+
     $("#p3").click(function(){
-        $("#p1").show();
-    })
-})
+        $("#p1").toggle();
+    });
+
+    $("p").hover(
+        function() { $(this).css("background-color", "#f8f9fa"); },
+        function() { $(this).css("background-color", "white"); }
+    );
+
+    $("form").focusin(function () {
+        $("label").addClass("bg-light text-primary rounded px-2");
+    });
+
+    $("#nom-err").hide();
+    
+    $("#nom").on('input', function(){
+        const nomSaisi = $(this).val().trim();
+        if(nomSaisi.length < 2){
+            $("#nom-err").show();
+        } else {
+            $("#nom-err").hide();
+        }
+    });
+});
